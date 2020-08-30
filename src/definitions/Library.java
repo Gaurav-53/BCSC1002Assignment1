7013c132bbdfbca15c7c223329225699204b1626
Gauarav varshney <gaurav.varshney_cs19@gla.ac.in>
2020-08-30 17:46:34 +0530

    add getter and setter method for fields for Library.java

--- src/definitions/Library.java
+++ src/definitions/Library.java
@@ -8,4 +8,12 @@ package definitions;
 
 public class Library {
     private Book[] currentlyAvailable;
+
+    public Book[] getCurrentlyAvailable() {
+        return currentlyAvailable.clone();
+    }
+
+    public void setCurrentlyAvailable(Book[] currentlyAvailable) {
+        this.currentlyAvailable = currentlyAvailable;
+    }
 }
